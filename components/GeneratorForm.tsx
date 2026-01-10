93ffbb34e4eb9d8585c0f3ee5f011946e80d7ca2
Igor <igorkmail@gmail.com>
2026-01-10 00:48:27 -0300

    feat: implements favicon

--- components/GeneratorForm.tsx
+++ components/GeneratorForm.tsx
@@ -173,10 +173,21 @@ export default function GeneratorForm() {
                         const newConfigs = digitConfigs.filter((_, i) => i !== index);
                         setDigitConfigs(newConfigs);
                       }}
-                      className="btn btn-error btn-sm mt-8"
+                      className="btn btn-error btn-sm mt-8 mb-2"
                       disabled={digitConfigs.length === 1}
                     >
-                      ❌
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="white"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
                     </button>
                   </div>
 
